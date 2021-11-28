f52aa9aee2b44a2e6b526c6444b95a7f96415a3f
Aryan Jabbari <AryanJabbari@gmail.com>
2021-11-28 23:20:51 +0000

    feat(ape-page): add name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,18 @@ const sanityClient = new SanityClient();
 const Home: NextPage<Props> = ({ apes }) => {
   return (
     <PageLayout>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 bg-green-50 p-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 bg-green-50 px-16 py-8">
         {apes.map((a) => {
           const heroImageUrl =
             sanityClient.urlForImageSource(a.image).auto('format').height(255).width(255).url() ??
             undefined;
           const xchainUrl = a.xchainUrl;
-          return xchainUrl ? (
-            <div key={heroImageUrl} className="flex flex-shrink">
-              <a href={xchainUrl} target="_blank" rel="noopener noreferrer">
+          return (
+            <div className="flex flex-col items-center" key={heroImageUrl}>
+              {a.name ? <p>{a.name}</p> : null}
+              <MaybeAnchor className="flex flex-shrink" href={xchainUrl}>
                 <img src={heroImageUrl} />
-              </a>
-            </div>
-          ) : (
-            <div key={heroImageUrl}>
-              <img src={heroImageUrl} />
+              </MaybeAnchor>
             </div>
           );
         })}
@@ -30,11 +27,23 @@ const Home: NextPage<Props> = ({ apes }) => {
   );
 };
 
+const MaybeAnchor: React.FC<{
+  className?: string;
+  href?: string;
+}> &
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement | HTMLAnchorElement>,
+    HTMLDivElement | HTMLAnchorElement
+  > = ({ href, ...props }) => {
+  if (href) return <a href={href} {...props} target="_blank" rel="noopener noreferrer" />;
+  return <div {...props} />;
+};
+
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const sanity = new SanityClient();
   const apes = await sanity.getApes();
   return { props: { apes } };
 };
-type Props = { apes: { image: string; xchainUrl?: string }[] };
+type Props = { apes: { image: string; name: string; xchainUrl?: string }[] };
 
 export default Home;
