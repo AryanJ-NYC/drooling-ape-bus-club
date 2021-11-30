e289ce02f1849643c447184f87a9a3572741b4aa
Aryan Jabbari <AryanJabbari@gmail.com>
2021-11-29 21:02:30 -0500

    style(card): restyle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const sanityClient = new SanityClient();
 const Home: NextPage<Props> = ({ apes }) => {
   return (
     <PageLayout>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 bg-green-50 px-16 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-16 py-8">
         {apes.map((a) => {
           const heroImageUrl =
             sanityClient.urlForImageSource(a.image).auto('format').height(255).width(255).url() ??
@@ -15,44 +15,46 @@ const Home: NextPage<Props> = ({ apes }) => {
           const xchainUrl = a.xchainUrl;
           return (
             <div className="flex flex-col items-center" key={heroImageUrl}>
-              {a.name ? <p>{a.name}</p> : null}
-              <MaybeAnchor className="flex flex-shrink" href={xchainUrl}>
-                <img src={heroImageUrl} />
-              </MaybeAnchor>
-              {a.artists?.length ? (
-                <p className="text-xs">
-                  by{' '}
-                  {a.artists.map((artist) => (
-                    <a
-                      className="text-blue-400 hover:text-blue-600"
-                      href={artist.webpage}
-                      key={artist.name}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {artist.name}
-                    </a>
-                  ))}
-                </p>
-              ) : null}
+              <div className="shadow-xl">
+                <img
+                  alt={`${a.name ?? 'unnamed'} asset`}
+                  className="bg-pink-50 rounded-t-md"
+                  height="255"
+                  width="255"
+                  src={heroImageUrl}
+                />
+                <div className="bg-pink-50 py-2 rounded-b-md flex flex-col items-center">
+                  {a.name ? <p className="tracking-wider">{a.name}</p> : null}
+                  {a.artists?.length ? (
+                    <p className="text-xs">
+                      by{' '}
+                      {a.artists.map((artist) => (
+                        <a
+                          className="text-blue-400 hover:text-blue-600"
+                          href={artist.webpage}
+                          key={artist.name}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {artist.name}
+                        </a>
+                      ))}
+                    </p>
+                  ) : null}
+                  <a
+                    className="cursor-pointer text-blue-400 hover:text-blue-600 text-xs"
+                    href={xchainUrl}
+                  >
+                    xchain
+                  </a>
+                </div>
+              </div>
             </div>
           );
         })}
       </div>
     </PageLayout>
   );
-};
-
-const MaybeAnchor: React.FC<{
-  className?: string;
-  href?: string;
-}> &
-  React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement | HTMLAnchorElement>,
-    HTMLDivElement | HTMLAnchorElement
-  > = ({ href, ...props }) => {
-  if (href) return <a href={href} {...props} target="_blank" rel="noopener noreferrer" />;
-  return <div {...props} />;
 };
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
