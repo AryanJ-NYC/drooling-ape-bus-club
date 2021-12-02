df90860892f96ed7a2fb2fc597c5a91cd3d6d4d3
Aryan Jabbari <AryanJabbari@gmail.com>
2021-12-01 20:31:57 -0500

    feat(logo): add

--- src/modules/shared/PageLayout.tsx
+++ src/modules/shared/PageLayout.tsx
@@ -1,14 +1,21 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { FaTelegramPlane } from 'react-icons/fa';
+import logo from '../../../public/logo.png';
 
 export const PageLayout: React.FC = ({ children }) => {
   return (
     <div>
       <header className="bg-pink-50 py-4">
-        <div className="flex items-center justify-between px-8 xl:px-44">
+        <div className="flex items-center justify-between px-8 md:px-24 xl:px-32">
           <Link href="/">
-            <a className="uppercase tracking-widest text-xl">Drooling Ape Bus Club</a>
+            <a className="flex items-end space-x-4">
+              <Image alt="Drooling Ape Bus Club logo" height="80" width="80" src={logo} />
+              <p className="hidden lg:block text-xl tracking-widest uppercase">
+                Drooling Ape Bus Club
+              </p>
+            </a>
           </Link>
           <a
             className="text-gray-400 hover:text-gray-700"
@@ -16,7 +23,7 @@ export const PageLayout: React.FC = ({ children }) => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <FaTelegramPlane />
+            <FaTelegramPlane className="h-10 w-10" />
           </a>
         </div>
       </header>
