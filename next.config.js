be3a44f7b2990ad7f0841cef9f1ea299754e951c
Aryan Jabbari <AryanJabbari@gmail.com>
2021-12-05 22:16:28 -0500

    feat(submit-page): add

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line
+const Yup = require('yup');
+
+const envVariablesValidationSchema = Yup.object({
+  telegramSubmissionChannelApiKey: Yup.string().required(),
+  telegramSubmissionChannelId: Yup.string().required(),
+});
+envVariablesValidationSchema.validateSync({
+  telegramSubmissionChannelApiKey: process.env.TELEGRAM_SUBMISSION_CHANNEL_TOKEN,
+  telegramSubmissionChannelId: process.env.TELEGRAM_SUBMISSION_CHANNEL_ID,
+});
+
 module.exports = {
   i18n: {
     locales: ['en'],
---
+++ src/pages/api/submit.ts
@@ -0,0 +1,31 @@
+import multer from 'multer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.TELEGRAM_SUBMISSION_CHANNEL_TOKEN;
+const bot = new TelegramBot(token);
+
+const upload = multer();
+const middleware = upload.single('file');
+
+const apiRouter = nextConnect({
+  onNoMatch(_, res: NextApiResponse) {
+    res.status(405).end();
+  },
+});
+apiRouter.use(middleware);
+apiRouter.post((req: NextApiRequest & { file: Express.Multer.File }, res: NextApiResponse) => {
+  bot.sendPhoto(process.env.TELEGRAM_SUBMISSION_CHANNEL_ID, req.file.buffer, {
+    caption: `${req.body.contact} has submitted ${req.body.apeName}`,
+  });
+  res.status(200).end();
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default apiRouter;
