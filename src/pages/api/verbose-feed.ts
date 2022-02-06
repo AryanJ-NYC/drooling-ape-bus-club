57d6f5362038cfc2de15b1c8fea893033dbf633f
Aryan Jabbari <AryanJabbari@gmail.com>
2022-02-06 17:57:07 -0500

    feat(verbose-feed): add order attribute

--- src/pages/api/verbose-feed.ts
+++ src/pages/api/verbose-feed.ts
@@ -1,16 +1,17 @@
 import { NextApiHandler } from 'next';
 import { SanityClient } from '../../sanity/client';
 
-const handler: NextApiHandler = async (req, res) => {
+const handler: NextApiHandler = async (_, res) => {
   const sanity = new SanityClient();
   const assets = await sanity.getApes();
   const massagedAssets = assets.reduce(
-    (prev, asset) => ({
+    (prev, asset, i) => ({
       ...prev,
       [asset.name]: {
         artists: asset.artists,
         asset: asset.name,
         img_url: sanity.urlForImageSource(asset.image).url(),
+        order: (i % 69) + 1,
         series: asset.series,
       },
     }),
