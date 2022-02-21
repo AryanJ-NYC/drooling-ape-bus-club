cb49edd07fb2166eb1b273b55b3ec9f7a1009161
Aryan Jabbari <AryanJabbari@gmail.com>
2022-02-21 10:40:29 -0500

    fix(verbose-feed): fix bad img_urls

--- src/pages/api/verbose-feed.ts
+++ src/pages/api/verbose-feed.ts
@@ -1,23 +1,30 @@
 import { NextApiHandler } from 'next';
 import { SanityClient } from '../../sanity/client';
+import { Ape } from '../../sanity/types';
 
+const sanity = new SanityClient();
 const handler: NextApiHandler = async (_, res) => {
-  const sanity = new SanityClient();
   const assets = await sanity.getApes();
-  const massagedAssets = assets.reduce(
-    (prev, asset, i) => ({
+  const massagedAssets = assets.reduce((prev, asset, i) => {
+    const img_url = getImageUrl(asset);
+    return {
       ...prev,
       [asset.name]: {
         artists: asset.artists,
         asset: asset.name,
-        img_url: asset.imageUrl ?? sanity.urlForImageSource(asset.image).url(),
+        img_url,
         order: (i % 69) + 1,
         series: asset.series,
       },
-    }),
-    {}
-  );
+    };
+  }, {});
   res.json(massagedAssets);
+};
+
+const getImageUrl = (asset: Ape): string => {
+  if (asset.imageUrl) return asset.imageUrl;
+  if (asset.image) return sanity.urlForImageSource(asset.image).url();
+  return '';
 };
 
 export default handler;
