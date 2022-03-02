280cf8b24d06165cfa51c4b7d8ac65db78dec5e5
Aryan Jabbari <AryanJabbari@gmail.com>
2022-03-01 19:18:17 -0500

    fix(verbose-feed): respond with the correct order number

--- src/pages/api/verbose-feed.ts
+++ src/pages/api/verbose-feed.ts
@@ -4,7 +4,15 @@ import { Ape } from '../../sanity/types';
 
 const sanity = new SanityClient();
 const handler: NextApiHandler = async (_, res) => {
-  const assets = await sanity.getApes();
+  const serieses = await sanity.getAllApesGroupedBySeries();
+
+  const assets: (Ape & { series: number })[] = [];
+  for (const series in serieses) {
+    const seriesNumber = Number(series) + 1;
+    const apes = await sanity.getApesBySeries(seriesNumber.toString());
+    assets.push(...apes.map((a) => ({ ...a, series: seriesNumber })));
+  }
+
   const massagedAssets = assets.reduce((prev, asset, i) => {
     const img_url = getImageUrl(asset);
     return {
