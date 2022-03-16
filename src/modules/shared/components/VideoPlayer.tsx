743d6dcef30e6f2137aa591d28097952e6e2ec20
Aryan Jabbari <AryanJabbari@gmail.com>
2022-03-16 13:16:36 -0400

    chore(video-player): fix height

--- src/modules/shared/components/VideoPlayer.tsx
+++ src/modules/shared/components/VideoPlayer.tsx
@@ -4,5 +4,9 @@ import { Player } from 'video-react';
 import '../../../../node_modules/video-react/dist/video-react.css';
 
 export const VideoPlayer: React.FC<{ src: string }> = ({ src }) => {
-  return <Player fluid playsInline src={src} />;
+  return (
+    <div>
+      <Player fluid playsInline src={src} />
+    </div>
+  );
 };
